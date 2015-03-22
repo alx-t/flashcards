3da052fa8d2e38f0d275674212f97357c37fa007
Alex Tesevich <atesevich@gmail.com>
2015-03-22 11:01:09 -0400

    third-task error correction 1

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,20 +1,17 @@
-class TranslatedTextValidator < ActiveModel::Validator
-  def validate(record)
-    if record.original_text.downcase == record.translated_text.downcase
-      record.errors[:translated_text] << "Must not be equal original text"
-    end
-  end
-end
-
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
-  validates_with TranslatedTextValidator
-
-  before_validation :set_review_date
+  validate :translated_text_not_equal_original
+  before_validation :set_review_date, on: :create
 
   protected
 
     def set_review_date
-      self.review_date = Time.now + 3.days
+      self.review_date = Time.now + 3.days if self.review_date.blank?
+    end
+
+    def translated_text_not_equal_original
+      if original_text.downcase == translated_text.downcase
+        errors[:translated_text] << "Must not be equal original text"
+      end
     end
 end
