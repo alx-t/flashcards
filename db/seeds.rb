e81e466b628edd462cc98f9240bb916e4ffbaeb7
Alex Tesevich <atesevich@gmail.com>
2015-03-22 14:54:13 -0400

    changing seed with nocogiri

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+=begin
 Card.create(original_text: 'Car', translated_text: 'Автомобиль', review_date: Time.now)
 Card.create(original_text: 'Cat', translated_text: 'Кот', review_date: Time.now)
 Card.create(original_text: 'Dog', translated_text: 'Собака', review_date: Time.now)
 Card.create(original_text: 'City', translated_text: 'Город', review_date: Time.now)
+=end
+
+require 'open-uri'
+require 'nokogiri'
+
+url = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
+
+doc = Nokogiri::HTML(open(url))
+items = doc.xpath("//td")
+index = 5
+step = 4
+while (index < items.length) do
+  original_text = items[index].text
+  translated_text = items[index+1].text
+  Card.create(original_text: original_text, translated_text: translated_text)
+  index += step
+end
