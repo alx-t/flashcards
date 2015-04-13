210148995c87e047e9260df7b71b0dae8f7634e0
Alex <atesevich@gmail.com>
2015-04-14 00:01:03 +0300

    Revert "Thirteenth task"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413131412) do
+ActiveRecord::Schema.define(version: 20150406190133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,13 +28,11 @@ ActiveRecord::Schema.define(version: 20150413131412) do
     t.string   "original_text"
     t.string   "translated_text"
     t.string   "review_date"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "user_id"
     t.string   "image"
     t.integer  "pack_id"
-    t.integer  "attempts",          default: 0
-    t.integer  "repetition_number", default: 0
   end
 
   add_index "cards", ["pack_id"], name: "index_cards_on_pack_id", using: :btree
