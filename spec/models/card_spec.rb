a9b58db7f8b08bdca4b7f3164cc2e9b950876bb9
Alex <atesevich@gmail.com>
2015-04-14 09:22:03 +0300

    Revert "Revert "Thirteenth task""

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -43,14 +43,6 @@ describe "Card" do
       expect(card.check_translation("TeSt")).to be true
     end
 
-    it "right answer" do
-      expect(card.check_translation("test")).to be true
-    end
-
-    it "wrong answer" do
-      expect(card.check_translation("book")).to be false
-    end
-
     context "russian text" do
       before(:each) { card.original_text = "тест" }
 
@@ -63,17 +55,101 @@ describe "Card" do
       end
     end
 
-    context "checking review date" do
-      before(:each) { card.review_date = Time.now - 3.days }
-
-      it "changing if right answer" do
+    context "after right answers" do
+      before(:each) do
+        card.review_date = Time.now - 3.days
         card.check_translation("test")
-        expect(card.review_date).to be >= Time.now
       end
 
-      it "not changing if wrong answer" do
+      it "attempts" do
+        expect(card.attempts).to eq(0)
+      end
+
+      it "repetition number" do
+        expect(card.repetition_number).to eq(1)
+      end
+
+      it "review date" do
+        expect(card.review_date).to be >= Time.now
+      end
+    end
+
+    context "after wrong answers" do
+      before(:each) do
+        card.review_date = Time.now - 3.days
         card.check_translation("book")
+      end
+
+      it "attempts" do
+        expect(card.attempts).to eq(1)
+      end
+
+      it "repetition number" do
+        expect(card.repetition_number).to eq(0)
+      end
+
+      it "review date" do
         expect(card.review_date).to be <= Time.now
+      end
+    end
+
+    context "wrong answer after 3 attempts" do
+      before(:each) do
+        card.review_date = Time.now - 3.days
+        card.attempts = 3
+        card.check_translation("book")
+      end
+
+      it "attempts" do
+        expect(card.attempts).to eq(0)
+      end
+
+      it "review date added 12 hours" do
+        expect(card.review_date >= Time.now + 12.hours - 1.minute).to be true
+      end
+
+      it "repetition number" do
+        expect(card.repetition_number).to eq(1)
+      end
+    end
+
+    context "right answer after 3 repetition" do
+      before(:each) do
+        card.review_date = Time.now - 3.days
+        card.repetition_number = 3
+        card.check_translation("test")
+      end
+
+      it "attempts" do
+        expect(card.attempts).to eq(0)
+      end
+
+      it "review date added 2 weeks" do
+        expect(card.review_date >= Time.now + 2.weeks - 1.minute).to be true
+      end
+
+      it "repetition number" do
+        expect(card.repetition_number).to eq(4)
+      end
+    end
+
+    context "right answer after 7 repetition" do
+      before(:each) do
+        card.review_date = Time.now - 3.days
+        card.repetition_number = 7
+        card.check_translation("test")
+      end
+
+      it "attempts" do
+        expect(card.attempts).to eq(0)
+      end
+
+      it "review date added 2 weeks" do
+        expect(card.review_date >= Time.now + 1.month - 1.minute).to be true
+      end
+
+      it "repetition number" do
+        expect(card.repetition_number).to eq(8)
       end
     end
   end
