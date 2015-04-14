311672f7a22ced42c0de8b65e196f41223148fee
Alex Tesevich <atesevich@gmail.com>
2015-04-14 21:46:37 +0300

    add levenshtein and codeclimate error correction 4

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,20 +1,7 @@
 class ReviewController < ApplicationController
   def check
-    card = current_user.cards.find(card_params[:card_id])
-    result = card.check_translation(card_params[:original_text])
-    case [result[:success], result[:typos_count]]
-    when [true, 0]
-      flash[:success] = "Правильно! \
-                         Слово: #{card.original_text}, \
-                         Перевод: #{card.translated_text}"
-    when [true, 1]
-      flash[:success] = "Правильно! \
-                         Слово: #{card.original_text}, \
-                         Перевод: #{card.translated_text}, \
-                         Ошибка ввода: #{card_params[:original_text]}"
-    else
-      flash[:danger] = "Не правильно!"
-    end
+    @card = current_user.cards.find(card_params[:card_id])
+    check_answer(@card.check_translation(card_params[:original_text]))
     redirect_to root_path
   end
 
@@ -22,5 +9,21 @@ class ReviewController < ApplicationController
 
   def card_params
     params.permit(:card_id, :original_text)
+  end
+
+  def check_answer(result)
+    case [result[:success], result[:typos_count]]
+    when [true, 0]
+      flash[:success] = "Правильно! \
+                         Слово: #{@card.original_text}, \
+                         Перевод: #{@card.translated_text}"
+    when [true, 1]
+      flash[:success] = "Правильно! \
+                         Слово: #{@card.original_text}, \
+                         Перевод: #{@card.translated_text}, \
+                         Ошибка ввода: #{card_params[:original_text]}"
+    else
+      flash[:danger] = "Не правильно!"
+    end
   end
 end
